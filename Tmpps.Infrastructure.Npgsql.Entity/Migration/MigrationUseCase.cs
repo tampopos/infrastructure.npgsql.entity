d751fe1175cea05d169f87f44d6463f5a8cf9503
wakuwaku3 <takushi_119@live.jp>
2018-12-09 03:10:13 +0900

    version up package

--- Tmpps.Infrastructure.Npgsql.Entity/Migration/MigrationUseCase.cs
+++ Tmpps.Infrastructure.Npgsql.Entity/Migration/MigrationUseCase.cs
@@ -31,6 +31,10 @@ namespace Tmpps.Infrastructure.Npgsql.Entity.Migration
         public async Task<int> ExecuteAsync()
         {
             var dir = this.pathResolver.ResolveDirectoryPath(this.config.Path);
+            if (string.IsNullOrEmpty(dir))
+            {
+                throw new BizLogicException($"指定のディレクトリーは存在しません。(path:{this.config.Path})");
+            }
             var databases = Directory.GetDirectories(dir).Where(x => string.IsNullOrEmpty(this.config.Database) || this.config.Database == Path.GetDirectoryName(x));
             foreach (var database in databases)
             {
